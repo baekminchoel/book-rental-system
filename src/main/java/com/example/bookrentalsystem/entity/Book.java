fcf94d0a3fe0bf9ebcd10c6cca189f52015c7875
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2025-01-20 18:19:21 +0900

    Add Entity Class

---
+++ src/main/java/com/example/bookrentalsystem/entity/Book.java
@@ -0,0 +1,29 @@
+package com.example.bookrentalsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter @Setter
+public class Book {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long id;
+
+    private String title;
+    private String author;
+    private String publisher;
+    private LocalDate publicationDate;
+
+    private int stock;  // 재고
+    private int rentedCount;    // 총 빌린 수량
+
+    private boolean available;  // 대출 가능 여부
+
+    private int overdueCount;   // 현재 연체된 도서 수
+}
